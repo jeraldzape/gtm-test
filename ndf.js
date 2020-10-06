6fd9d0de12c18dd8c696c3964955b357f128a172
Jerald <jeraldzape@gmail.com>
2020-10-06 13:44:03 +0800

    Added files

--- ndf.js
+++ ndf.js
@@ -1,1 +1,3 @@
-console.log("This is from NDF script (external)");
+console.log("** This is from NDF script (external)");
+window['optimizely'] = 'some optimizely content';
+console.log(window['optimizely']);
